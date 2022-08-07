4f5191ee1e0bf4552189a89f1c4609d79fee5872
alecs6k <76454501+alecs6k@users.noreply.github.com>
2022-08-07 19:55:38 -0400

    update

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ app.use(express.json());
 
 // configurar cabeceras http
 app.use((req, res, next) => {
+    res.header("*");
+    res.header("GET", "PUT", "POST", "DELETE");
+    res.header("Accept", "application/json");
+    res.header("Content-Type", "application/json");
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Accept', 'application/json');
-    res.header('Content-Type', 'application/json');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
  
     next();
 });
